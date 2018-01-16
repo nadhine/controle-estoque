23dd057b3c9eac8384658ca9f10548e2fec0b5c6
Nadhine <hangrel@gmail.com>
2018-01-16 13:45:33 -0300

    commit inicial

---
+++ home.php
@@ -0,0 +1,75 @@
+<?php
+
+	require_once("session.php");
+
+	require_once("class.user.php");
+	$auth_user = new USER();
+
+
+	$user_id = $_SESSION['user_session'];
+
+	$stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
+	$stmt->execute(array(":user_id"=>$user_id));
+
+	$userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<script type="text/javascript" src="jquery-1.11.3-jquery.min.js"></script>
+<link rel="stylesheet" href="style.css" type="text/css"  />
+<title>Bem vindo, - <?php print($userRow['user_email']); ?></title>
+</head>
+
+<body>
+
+<nav class="navbar">
+      <div class="tabbable">
+          <ul class="nav nav-tabs">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+				<span class="glyphicon glyphicon-user"></span>&nbsp;Olá,  <?php echo $userRow['user_email']; ?>&nbsp;<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;Ver Perfil</a></li>
+								<li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sair</a></li>
+							</ul>
+						</li>
+            <li class="active"><a href="#produtos" data-toggle="tab">Produtos</a></li>
+            <li><a href="#fornecedores" data-toggle="tab">Fornecedores</a></li>
+            <li><a href="#movimentos" data-toggle="tab">Movimentações</a></li>
+          </ul>
+      </div>
+</nav>
+
+<div class="tab-content" style="margin-top:80px;">
+
+    <div class="tab-pane active" id="produtos">
+			<div class="embed-responsive embed-responsive-16by9">
+			    <iframe class="embed-responsive-item" src="newproduct.php" allowfullscreen></iframe>
+			</div>
+    </div>
+
+		<div class="tab-pane" id="fornecedores">
+			<div class="embed-responsive embed-responsive-16by9">
+			    <iframe class="embed-responsive-item" src="newprovider.php" allowfullscreen></iframe>
+			</div>
+		</p>
+		</div>
+
+		<div class="tab-pane " id="movimentos">
+			<div class="embed-responsive embed-responsive-16by9">
+			    <iframe class="embed-responsive-item" src="newmovement.php" allowfullscreen></iframe>
+			</div>
+		</div>
+
+</div>
+<p class="blockquote-reverse" style="margin-top:200px;">
+Programming Blog Featuring Tutorials on PHP, MySQL, Ajax, jQuery, Web Design and More...<br /><br />
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ sign-up.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+require_once('class.user.php');
+$user = new USER();
+
+if($user->is_loggedin()!="")
+{
+	$user->redirect('home.php');
+}
+
+if(isset($_POST['btn-signup']))
+{
+	$uname = strip_tags($_POST['txt_uname']);
+	$umail = strip_tags($_POST['txt_umail']);
+	$upass = strip_tags($_POST['txt_upass']);
+
+	if($uname=="")	{
+		$error[] = "Cadastre um nome de usuário!";
+	}
+	else if($umail=="")	{
+		$error[] = "Cadastre um email único!";
+	}
+	else if(!filter_var($umail, FILTER_VALIDATE_EMAIL))	{
+	    $error[] = 'Por favor cadastre um email válido!';
+	}
+	else if($upass=="")	{
+		$error[] = "Cadastre uma senha";
+	}
+	else if(strlen($upass) < 6){
+		$error[] = "Sua senha deve ter pelo menos seis caracteres";
+	}
+	else
+	{
+		try
+		{
+			$stmt = $user->runQuery("SELECT user_name, user_email FROM users WHERE user_name=:uname OR user_email=:umail");
+			$stmt->execute(array(':uname'=>$uname, ':umail'=>$umail));
+			$row=$stmt->fetch(PDO::FETCH_ASSOC);
+
+			if($row['user_name']==$uname) {
+				$error[] = "Desculpe, esse nome já cadastrado";
+			}
+			else if($row['user_email']==$umail) {
+				$error[] = "Desculpe, email já cadastrado";
+			}
+			else
+			{
+				if($user->register($uname,$umail,$upass)){
+					$user->redirect('sign-up.php?joined');
+				}
+			}
+		}
+		catch(PDOException $e)
+		{
+			echo $e->getMessage();
+		}
+	}
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Cadastro</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" href="style.css" type="text/css"  />
+</head>
+<body>
+
+<div class="signin-form">
+
+<div class="container">
+
+        <form method="post" class="form-signin">
+            <h2 class="form-signin-heading">Cadastro de Usuário</h2><hr />
+            <?php
+			if(isset($error))
+			{
+			 	foreach($error as $error)
+			 	{
+					 ?>
+                     <div class="alert alert-danger">
+                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?>
+                     </div>
+                     <?php
+				}
+			}
+			else if(isset($_GET['joined']))
+			{
+				 ?>
+                 <div class="alert alert-info">
+                      <i class="glyphicon glyphicon-log-in"></i> &nbsp; Cadastro realizado com sucesso <a href='index.php'>login</a> here
+                 </div>
+                 <?php
+			}
+			?>
+            <div class="form-group">
+            <input type="text" class="form-control" name="txt_uname" placeholder="Nome de Usuário" value="<?php if(isset($error)){echo $uname;}?>" />
+            </div>
+            <div class="form-group">
+            <input type="text" class="form-control" name="txt_umail" placeholder="Email" value="<?php if(isset($error)){echo $umail;}?>" />
+            </div>
+            <div class="form-group">
+            	<input type="password" class="form-control" name="txt_upass" placeholder="Senha" />
+            </div>
+            <div class="clearfix"></div><hr />
+            <div class="form-group">
+            	<button type="submit" class="btn btn-primary" name="btn-signup">
+                	<i class="glyphicon glyphicon-open-file"></i>&nbsp;CADASTRAR
+                </button>
+            </div>
+            <br />
+            <label>Já possui cadastro? <a href="index.php">Entrar</a></label>
+        </form>
+       </div>
+</div>
+
+</div>
+
+</body>
+</html>
